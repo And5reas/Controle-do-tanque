13e91343f7cf8b2ccaeb6019a89bfb463208fd40
and5reas <and5reas.porcel@hotmail.com>
2025-04-06 17:42:48 -0300

    feat Tudo Certo

--- src/main.cpp
+++ src/main.cpp
@@ -9,12 +9,13 @@ const char* password = passwordSecret;
 
 // Configuração do MQTT
 const char* mqttServer = "test.mosquitto.org";
-const char topico[36] = "projeto/tanque/216590/215446/214707";
+const char topico[36] = "projeto/tanque/216590";
 
 const int trigPin = 5;
 const int echoPin = 18;
 const int bombaPin = 33;
 const int solenoidePin = 32;
+const int statusOnlinePin = 2;
 
 //define sound speed in cm/uS
 #define SOUND_SPEED 0.034
@@ -26,34 +27,75 @@ static unsigned long lastMsg = 0;
 
 long duration;
 float distanceCm;
+float funPorcentagem;
+bool automatico;
+
+void calc() {
+  funPorcentagem = ((45/11)*distanceCm)-(250/11);
+
+  if (funPorcentagem > 85 && automatico) {
+    digitalWrite(bombaPin, HIGH);
+    digitalWrite(solenoidePin, LOW);
+  } else
+  if (funPorcentagem < 15 && automatico) {
+    digitalWrite(bombaPin, LOW);
+  }
+}
 
 // Função para publicar um JSON
 void sendJson() {
-  JsonDocument doc;  // Cria um JSON de até 200 bytes
+  // JsonDocument doc;  // Cria um JSON de até 200 bytes
   
-  doc["nivel"] = distanceCm;
+  // doc["nivel"] = distanceCm;
 
-  char jsonBuffer[512];
-  serializeJson(doc, jsonBuffer); // Converte JSON para string
+  // char jsonBuffer[512];
+  // serializeJson(doc, jsonBuffer); // Converte JSON para string
 
-  client.publish(topico, jsonBuffer);  // Envia JSON via MQTT
-  Serial.println("JSON enviado:");
-  Serial.println(jsonBuffer);
+  char stringNivel[10]; 
+  sprintf(stringNivel, "%.0f", funPorcentagem);
+  client.publish(topico, stringNivel);
+  
+  //Serial.println("JSON enviado:");
+  //Serial.println(jsonBuffer
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Mensagem recebida no tópico: ");
-  Serial.println(topic);
+  // Serial.print("Mensagem recebida no tópico: ");
+  // Serial.println(topic);
 
-  Serial.print("Mensagem: ");
+  String message = "";
   for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
+    message += (char)payload[i];
   }
-  Serial.println();
+  // Serial.print("Mensagem: " + message);
+
+  if (message == "toggleBomba" && !automatico) {
+    if (digitalRead(bombaPin)) {
+      digitalWrite(bombaPin, LOW);
+    } else {
+      digitalWrite(bombaPin, HIGH);
+    }
+  }
+
+  if (message == "toggleSolenoide" && !automatico) {
+    if (digitalRead(solenoidePin)) {
+      digitalWrite(solenoidePin, LOW);
+    } else {
+      digitalWrite(solenoidePin, HIGH);
+    }
+  }
+
+  if (message == "toggleAuto") {
+    automatico = !automatico;
+  }
+
+  // Serial.println();
+  // Serial.println();
 }
 
 void reconnect() {
   while (!client.connected()) {
+    digitalWrite(2, LOW);
     Serial.print("Tentando conexão MQTT...");
     if (client.connect("andreassss")) {  // Nome do cliente MQTT
       Serial.println("Conectado!");
@@ -81,26 +123,6 @@ void ConectWifi() {
   Serial.print("Got IP: ");  Serial.println(WiFi.localIP());
 }
 
-void setup() {
-  Serial.begin(115200);
-
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  pinMode(echoPin, INPUT);
-  pinMode(trigPin, OUTPUT);
-  pinMode(bombaPin, OUTPUT);
-  pinMode(solenoidePin, OUTPUT);
-
-  ConectWifi();
-
-  // Configurar o cliente MQTT
-  client.setServer(mqttServer, 1883);
-  client.setCallback(callback);
-
-  reconnect();
-}
-
 void hcsr04() {
   // Clears the trigPin
   digitalWrite(trigPin, LOW);
@@ -120,18 +142,48 @@ void hcsr04() {
   delay(200);
 }
 
+void setup() {
+  Serial.begin(115200);
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(bombaPin, OUTPUT);
+  pinMode(solenoidePin, OUTPUT);
+  pinMode(statusOnlinePin, OUTPUT);
+
+  automatico = true;
+
+  ConectWifi();
+
+  // Configurar o cliente MQTT
+  client.setServer(mqttServer, 1883);
+  client.setCallback(callback);
+
+  reconnect();
+}
+
 void loop() {
   client.loop();
-  // Publica uma mensagem a cada 5 segundos
-  if ((millis() - lastMsg) > 5000) {
-    lastMsg = millis();
-    hcsr04();
-    sendJson();
+
+  if (client.connected()) {
+    digitalWrite(2, HIGH);
   }
 
-  if (distanceCm < 10) {
-    digitalWrite(bombaPin, HIGH);
-  } else {
-    digitalWrite(bombaPin, LOW);
+  reconnect();
+
+  // Publica uma mensagem a cada 7 segundos
+  if ((millis() - lastMsg) > 3000) {
+    lastMsg = millis();
+    hcsr04();
+    calc();
+    // Serial.println("  |  " + (String)distanceCm);
+    if (client.connected()) {
+      sendJson();
+    } else {
+      digitalWrite(2, LOW);
+    }
   }
 }
